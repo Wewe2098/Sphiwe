5e5b4bdab44557821d8431a0e67d34e0ae5ac2dd
Sphiwe Mkwatheni <43848182+Wewe2098@users.noreply.github.com>
2023-07-27 20:41:52 +0000

    add multiplication method

--- task1_project/helloWorld.js
+++ task1_project/helloWorld.js
@@ -10,8 +10,13 @@ class Calculator {
   subtract(num1, num2) {
     return num1 - num2;
   }
+
+  multiply(num1, num2) {
+    return num1 * num2;
+  }
 }
 
 const calculator = new Calculator();
 console.log(calculator.add(5, 3)); // Output: 8
 console.log(calculator.subtract(5, 3)); // Output: 2
+console.log(calculator.multiply(5, 3)); // Output: 15
