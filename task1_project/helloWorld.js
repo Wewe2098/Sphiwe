6935b1092ae5b9ac4c6a69841174c62ad08d7382
Sphiwe Mkwatheni <43848182+Wewe2098@users.noreply.github.com>
2023-07-27 20:34:26 +0000

    new changes

--- task1_project/helloWorld.js
+++ task1_project/helloWorld.js
@@ -1,18 +1,22 @@
 class Calculator {
-    constructor() {
-      // constructor logic
-    }
-  
-    add(num1, num2) {
-      return num1 + num2;
-    }
-  
-    subtract(num1, num2) {
-      return num1 - num2;
-    }
+  constructor() {
+    // constructor logic
   }
-  
-  const calculator = new Calculator();
-  console.log(calculator.add(5, 3)); 
-  console.log(calculator.subtract(5, 3)); 
-  
+
+  add(num1, num2) {
+    return num1 + num2;
+  }
+
+  subtract(num1, num2) {
+    return num1 - num2;
+  }
+
+  multiply(num1, num2) {
+    return num1 * num2;
+  }
+}
+
+const calculator = new Calculator();
+console.log(calculator.add(5, 3)); // Output: 8
+console.log(calculator.subtract(5, 3)); // Output: 2
+console.log(calculator.multiply(5, 3)); // Output: 15
