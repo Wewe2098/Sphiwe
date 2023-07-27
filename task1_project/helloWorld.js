1092d590ca6576d9bab3b955204ce9591d36861c
Sphiwe Mkwatheni <43848182+Wewe2098@users.noreply.github.com>
2023-07-27 20:28:37 +0000

    original code

--- task1_project/helloWorld.js
+++ task1_project/helloWorld.js
@@ -13,6 +13,6 @@ class Calculator {
   }
   
   const calculator = new Calculator();
-  console.log(calculator.add(5, 3)); // Output: 8
-  console.log(calculator.subtract(5, 3)); // Output: 2
+  console.log(calculator.add(5, 3)); 
+  console.log(calculator.subtract(5, 3)); 
   
