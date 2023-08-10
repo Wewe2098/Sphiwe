07038e8a95f838872093f8000bd228b3e1a128fe
Sphiwe Mkwatheni <43848182+Wewe2098@users.noreply.github.com>
2023-08-10 17:41:58 +0000

    new changes

--- online-cv/src/components/Home.js
+++ online-cv/src/components/Home.js
@@ -3,9 +3,9 @@ import Profile from "../components/Profile";
 import Contact from "../components/Contact";
 import About from "../components/About";
 import Skills from "../components/Skills";
-import Experience from "../components/Experiences";
-import Education from "../components/Educations";
-import Portfolio from "../components/Portfolios";
+import Experiences from "../components/Experiences";
+import Educations from "../components/Educations";
+import Portfolios from "../components/Portfolios";
 
 export default class Home extends Component {
   render() {
@@ -14,8 +14,15 @@ export default class Home extends Component {
         <div className='container'>
             <div className='row'>
                 <div className='col s12 m6 l4 light-grey darken-4' >
+                  <Profile />
+                  <Contact />
+                  <Skills />
                 </div>
                 <div className='col s12 m12 l9 white'>
+                  <About />
+                  <Educations />
+                  <Experiences />
+                  <Portfolios />
                 </div>
             </div>
         </div>
