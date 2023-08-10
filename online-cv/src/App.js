59f035adac13b4fec49239edeae84f238e140b5a
Sphiwe Mkwatheni <43848182+Wewe2098@users.noreply.github.com>
2023-08-10 17:29:38 +0000

    new changes

--- online-cv/src/App.js
+++ online-cv/src/App.js
@@ -2,25 +2,18 @@ import React from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home.js';
-import Skills from './components/Skills.js';
-import Educations from './components/Educations.js';
-import Experiences from './components/Experiences.js';
-import Portfolios from './components/Portfolios.js';
+
 function App() {
    return (
-     <Router>
-         <>
-           <Route exact path="/" component={Home} />
-           <Switch>
-               <Route path="/skills" component={Skills} />
-               <Route path="/educations" component={Educations} />
-               <Route path="/experiences" component={Experiences} />
-               <Route path="/portfolios" component={Portfolios} />
-           </Switch>
-         </>
-     </Router>
+     <div className='App'>
+      <header className='App-header'>
+        <img src='logo' className='App-logo' alt='logo' />
+        <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>learn React</a>
+      </header>
+     </div>
    );
 }
 export default App;
--- online-cv/src/components/Home.js
+++ online-cv/src/components/Home.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
+import Profile from "../components/Profile";
+import Contact from "../components/Contact";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Experience from "../components/Experiences";
+import Education from "../components/Educations";
+import Portfolio from "../components/Portfolios";
 
 export default class Home extends Component {
   render() {
     return (
       <section>
-        <Navbar />
         <div className='container'>
             <div className='row'>
-                <div className='col s12 m4 l3'>
-                    <Profile />
+                <div className='col s12 m6 l4 light-grey darken-4' >
                 </div>
-                <div className='col s12 m8 l9'>
-                    <About />
-                    <Skills />
-                    <Experiences />
-                    <Educations />
-                    <Portfolios />
+                <div className='col s12 m12 l9 white'>
                 </div>
             </div>
         </div>
