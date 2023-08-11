cb682f2a089cfb89bff0176f6069a0bdf62f998a
Sphiwe Mkwatheni <43848182+Wewe2098@users.noreply.github.com>
2023-08-11 09:28:54 +0000

    new changes

--- online-cv/src/components/Experiences.js
+++ online-cv/src/components/Experiences.js
@@ -3,7 +3,31 @@ import React, { Component } from 'react'
 export default class Experiences extends Component {
   render() {
     return (
-      <div>Experiences</div>
+      <div>
+        <div className='card z-depth-0'>
+          <div className='card-content'>
+              <h6>
+                <strong>
+                  <i className='fas fa-tools'></i> EXPERIENCE
+                </strong>
+              </h6>
+              <hr />
+              <div className='row mt'>
+                <div className='col s12'>
+                  <blockquote className='content-right'>
+                    <h6 className='no-pad mt-bottom'>
+                      <strong>WEB DEVELOPER</strong>
+                      <span>2023 - Till Date</span>
+                    </h6>
+                    <p className='pt'>
+                      
+                    </p>
+                  </blockquote>
+                </div>
+            </div>
+        </div>
+        </div>
+      </div>
     )
   }
 }
--- online-cv/src/components/Home.js
+++ online-cv/src/components/Home.js
@@ -3,8 +3,8 @@ import Profile from "../components/Profile";
 import Contact from "../components/Contact";
 import About from "../components/About";
 import Skills from "../components/Skills";
-import Experiences from "../components/Experiences";
 import Educations from "../components/Educations";
+import Experiences from "../components/Experiences";
 import Portfolios from "../components/Portfolios";
 
 export default class Home extends Component {
