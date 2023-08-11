68d7362e2ad63e24728a2c92684c40bf015c2898
Sphiwe Mkwatheni <43848182+Wewe2098@users.noreply.github.com>
2023-08-11 09:11:35 +0000

    new staged changes

--- online-cv/src/components/Home.js
+++ online-cv/src/components/Home.js
@@ -7,24 +7,24 @@ import Experiences from "../components/Experiences";
 import Educations from "../components/Educations";
 import Portfolios from "../components/Portfolios";
 
-export default class Home extends Components {
+export default class Home extends Component {
   render() {
     return (
       <section>
-            <div className='row'>
-                <div className='col s12 m6 l4 light-grey darken-4' >
-                  <Profile />
-                  <Contact />
-                  <Skills />
-                </div>
-                <div className='col s12 m12 l9 white'>
-                  <About />
-                  <Educations />
-                  <Experiences />
-                  <Portfolios />
-                </div>
-            </div>
+        <div className='row'>
+          <div className='col s12 m6 l4 light-grey darken-4'>
+            <Profile />
+            <Contact />
+            <Skills />
+          </div>
+          <div className='col s12 m6 l8 white'>
+            <About />
+            <Educations />
+            <Experiences />
+            <Portfolios />
+          </div>
+        </div>
       </section>
-    )
+    );
   }
 }
